9d1fd73a4fe190eb8fea89c6b19151cdede220a6
Diego Bezerra <diegobmartins21@gmail.com>
2025-05-08 21:47:24 -0300

    challenge: profileCard component, I've improved the challenge by insert js modules and mocked infos

--- 01-basics/pizza-menu/src/index.js
+++ 01-basics/pizza-menu/src/index.js
@@ -1,22 +1,96 @@
 import React from "react";
 import ReactDom from "react-dom/client";
 
+import { ProfileCard } from "./components/challenges/profileCard/ProfileCard";
+
+import { userData } from "./components/challenges/profileCard/mock";
+
+import "./index.css";
+
 function App() {
   return (
-    <div>
-      <h1>Hello React</h1>
-      <Pizza />
+    <div className="container">
+      <Header />
+      <Menu />
+      {userData.map((data) => {
+        return <ProfileCard userData={data} key={data.userId} />;
+      })}
+      <Footer />
     </div>
   );
 }
 
-function Pizza() {
+function Pizza(props) {
+  const { pizzaName, ingredients, price, photoName, soldOut } = props;
+
   return (
-    <div>
-      <img src="/pizzas/focaccia.jpg " alt="Pizza Fogaccia" />
-      <h3>Focaccia</h3>
-      <h4>Ingredients</h4>
+    <div className="pizza">
+      <div>
+        <img src={photoName} alt={`Pizza ${pizzaName}`} />
+        <h3>{pizzaName}</h3>
+        <p>{ingredients}</p>
+        <span>$ {price}</span>
+        <p>{!soldOut ? "Available" : "Sold out"}</p>
+      </div>
     </div>
+  );
+}
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>Fast React Pizza Co.</h1>
+    </header>
+  );
+}
+
+function Menu() {
+  // name: "Pizza Spinaci",
+  //   ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+  //   price: 12,
+  //   photoName: "pizzas/spinaci.jpg",
+  //   soldOut: false,
+
+  return (
+    <div className="menu">
+      <h2>Pizza Menu</h2>
+      <div className="pizzas">
+        <Pizza
+          pizzaName="Focaccia"
+          ingredients="Bread with italian olive oil and rosemary"
+          price={6}
+          photoName="pizzas/focaccia.jpg"
+          soldOut={false}
+        />
+        <Pizza
+          pizzaName="Pizza Margherita"
+          ingredients="Tomato and mozarella"
+          price={10}
+          photoName="pizzas/margherita.jpg"
+          soldOut={false}
+        />
+        <Pizza
+          pizzaName="Pizza Spinaci"
+          ingredients="Tomato, mozarella, spinach, and ricotta cheese"
+          price={12}
+          photoName="pizzas/spinaci.jpg"
+          soldOut={false}
+        />
+      </div>
+    </div>
+  );
+}
+
+function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+
+  return (
+    <footer className="footer">
+      {isOpen ? "we're currently open" : "we're closed"}
+    </footer>
   );
 }
 
